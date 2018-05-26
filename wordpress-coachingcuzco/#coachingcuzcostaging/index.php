c5743afef0d853d8f078e9fbb4308cb7c75d57c1
Naomi Zuiverloon <naomizuiverloon@macbook-pro-2.home>
2018-05-26 15:14:24 +0200

    adding coaching-cuzco files

---
+++ wordpress-coachingcuzco/#coachingcuzcostaging/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Under Construction/En Construccion | Coaching and Management Consultancy Cuzco S.A.C.</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <header>
+    <div class="title">
+      <h1>Under construction<br>En construcci&oacute;n</h1>
+    </div>
+  </header>
+
+  <main>
+    <img src="logo-coachingcuzco-white-m.png" alt="Logo Coaching Cuzco">
+    <div class="contact">
+      <p style="font-weight:700">Coaching and Management Consultancy Cuzco S.A.C.</p>
+      <p>Anna Kitselaar</p>
+      <p><i class="fas fa-phone svg"></i>(+51) 978 621 394</p>
+      <p><i class="fas fa-envelope svg"></i><a href="mailto:anna@coachingbonaire.com">anna@coachingcuzco.com</a></p>
+    </div>
+  </main>
+
+  <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/solid.js" integrity="sha384-P4tSluxIpPk9wNy8WSD8wJDvA8YZIkC6AQ+BfAFLXcUZIPQGu4Ifv4Kqq+i2XzrM" crossorigin="anonymous"></script>
+  <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/fontawesome.js" integrity="sha384-2IUdwouOFWauLdwTuAyHeMMRFfeyy4vqYNjodih+28v2ReC+8j+sLF9cK339k5hY" crossorigin="anonymous"></script>
+</body>
+
+</html>
