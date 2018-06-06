16a62e6224ffb1ae97b0e230af933477f411fb78
Naomi Zuiverloon <naomizuiverloon@macbook-pro-2.home>
2018-06-06 22:31:39 +0200

    updated nav

--- wordpress-coachingcuzco/resources/js/script.js
+++ wordpress-coachingcuzco/resources/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  // Scroll on buttons - method 1
+  // Scroll on buttons
   $('.js--scroll-to-contact').click(function () {
     // the animation scrolls to the top of the js--section-plans with a speed of 1000ms
     $('html, body').animate({scrollTop: $('.js--section-contact').offset().top}, 1000)
@@ -8,41 +8,49 @@ $(document).ready(function() {
 
   // Navigation scroll
   // Select all links with hashes
-  $('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function(event) {
-      // On-page links
-      if (
-        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-        &&
-        location.hostname == this.hostname
-      ) {
-        // Figure out element to scroll to
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        // Does a scroll target exist?
-        if (target.length) {
-          // Only prevent default if animation is actually gonna happen
-          event.preventDefault();
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000, function() {
-            // Callback after animation
-            // Must change focus!
-            var $target = $(target);
-            $target.focus();
-            if ($target.is(":focus")) { // Checking if the target was focused
-              return false;
-            } else {
-              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-              $target.focus(); // Set focus again
-            };
-          });
+    $('a[href*="#"]')
+      // Remove links that don't actually link to anything
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function(event) {
+        // Grab height of fixed navigation
+        var navHeight = $('nav').outerHeight();
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top - 80
+            }, 1000, function() {
+              // Callback after animation
+              // Don't scroll on focus!
+                $.fn.focusNoScroll = function(){
+                  var x = window.scrollX, y = window.scrollY;
+                  this.focus();
+                  window.scrollTo(x, y);
+                  return this; //chainability
+
+                };
+                // Must change focus, but don't scroll!
+              var $target = $(target);
+              $target.focusNoScroll();
+              if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focusNoScroll(); // Set focus again
+              };
+            });
+          }
         }
-      }
-    });
+      });
 
 
 
@@ -59,25 +67,53 @@ $(document).ready(function() {
 
 
   // Mobile Navigation
-  $('.js--nav-icon').click(function() {
+  // Clicking on the hamburger menu and a link inside the menu fires slideToggle event
+  $('.js--nav-icon, .js--nav-menu a').click(function(element) {
     var nav = $('.js--nav-menu');
     var icon = $('.js--nav-icon>i');
+    var logo = $('.jumbotron');
+
+    //Gets the class name of the element that triggered the event
+  	//var clicked = element.target.className;
+
+ 		//Exits the function if the menu is closed
+    // if (icon.hasClass('fa-bars'))
+    //   return;
 
     // Toggle the menu box open in 0.2 seconds
-    if ($(window).width() < 768){
-      nav.slideToggle(200);
+    if ($(window).width() < 846){
+      nav.slideToggle(200)
     }
 
     // Remove hamburger icon and add cross
     if (icon.hasClass('fa-bars')) {
       icon.addClass('fa-times');
       icon.removeClass('fa-bars');
+      // logo.css("display","none");
+
     } else {
       // Remove cross icon and add hamburger
       icon.addClass('fa-bars');
       icon.removeClass('fa-times');
-    }
+      // logo.css("display","initial");
 
+    }
   });
+
+  $(window).resize(function() {
+  var nav = $('.js--nav-menu');
+  var icon = $('.js--nav-icon i');
+
+  if ($(window).width() > 767) {
+    nav.css("display", "block");
+    icon.addClass('fa-times');
+    icon.removeClass('fa-bars');
+  } else {
+    nav.css("display", "none");
+    icon.addClass('fa-bars');
+    icon.removeClass('fa-times');
+  }
+
+	});
 
 });
